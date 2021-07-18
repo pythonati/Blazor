329aff4b9f440b7b876711cfba002dd216ba87bc
pythonati@gmail.com <pythonati@gmail.com>
2021-07-18 10:10:07 +0300

    JS

---
+++ Features/Dashboard/ChartData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstBlazor.Features.Dashboard
+{
+    public class ChartData
+    {
+        public List<string> Labels { get; set; } = new();
+        public List<DataSet> Datasets { get; set; } = new();
+    }
+
+    public class DataSet
+    {
+        public string Label { get; set; }
+        public List<int> Data { get; set; } = new();
+        public List<string> BackgroundColor { get; set; } = new();
+        public List<string> BorderColor { get; set; } = new();
+        public int BorderWidth { get; set; }
+    }
+}
---
+++ wwwroot/js/chart-wrapper.js
@@ -0,0 +1,22 @@
+﻿window.blazoredRepairs = {
+
+    buildChart: (element, type, data) => {
+        var chart = new Chart(element, {
+            type: type,
+            data: data,
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            stepSize: 1
+                        }
+                    }]
+                },
+                legend: {
+                    display: false
+                }
+            }
+        });
+    }
+};
